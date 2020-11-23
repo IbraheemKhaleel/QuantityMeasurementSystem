62265c44ea4dbf667fa7771ef2db2d75602e06f9
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-23 11:45:52 +0530

    [Ibraheem Khaleel]Refactored : code for creating methods and constants to compute quantity measurement of length, weight and temperature.

--- main_quantity_measurement_system/quantity_measurement_system.py
+++ main_quantity_measurement_system/quantity_measurement_system.py
@@ -7,10 +7,17 @@ logging.basicConfig(filename='quantity_measurement_system.log', level=logging.DE
 
 
 class QuantityMeasurementSystem(IQuantityMeasurementSystem):
+
     """
     Created a class to compute quantity measurement of length, weight and temperature
     IQuantityMeasurementSystem is the interface
     """
+
+    METER_TO_CENTIMETER = 100
+    METER_TO_KILOMETER = .001
+    GRAMS_TO_KILOGRAM = .001
+    GRAMS_TO_MILLIGRAM = 1000
+    KELVIN_TEMPERATURE = 273.15
 
     def length_meter_to_centimeter_conversion(self, input_length):
         """
@@ -18,7 +25,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted centimeter length
         """
         try:
-            centi_meter_length = round((input_length * 100), 2)
+            centi_meter_length = round((input_length * self.METER_TO_CENTIMETER), 2)
             logging.debug("centimeter length is {}".format(centi_meter_length))
             return centi_meter_length
         except TypeError:
@@ -31,7 +38,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted kilometer length
         """
         try:
-            kilo_meter_length = round((input_length * .001), 2)
+            kilo_meter_length = round((input_length * self.METER_TO_KILOMETER), 2)
             logging.debug("kilometer length is {}".format(kilo_meter_length))
             return kilo_meter_length
         except TypeError:
@@ -44,7 +51,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted kilogram weight
         """
         try:
-            kilo_gram_weight = round((input_weight * .001), 2)
+            kilo_gram_weight = round((input_weight * self.GRAMS_TO_KILOGRAM), 2)
             logging.debug("kilogram weight is {}".format(kilo_gram_weight))
             return kilo_gram_weight
         except TypeError:
@@ -58,7 +65,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted milligram weight
         """
         try:
-            milli_gram_weight = round((input_weight * 1000), 2)
+            milli_gram_weight = round((input_weight *  self.GRAMS_TO_MILLIGRAM), 2)
             logging.debug("milli gram weight is {}".format(milli_gram_weight))
             return milli_gram_weight
         except TypeError:
@@ -97,7 +104,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted kelvin temperature
         """
         try:
-            kelvin_temperature = round((input_temperature + 273.15), 2)
+            kelvin_temperature = round((input_temperature + self.KELVIN_TEMPERATURE), 2)
             logging.debug("kelvin temperature is {}".format(kelvin_temperature))
             return kelvin_temperature
         except TypeError:
@@ -110,7 +117,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted celsius temperature
         """
         try:
-            celsius_temperature = round((input_temperature - 273.15), 2)
+            celsius_temperature = round((input_temperature - self.KELVIN_TEMPERATURE), 2)
             logging.debug("celsius temperature is {}".format(celsius_temperature))
             return celsius_temperature
         except TypeError:
@@ -123,7 +130,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted kelvin temperature
         """
         try:
-            kelvin_temperature = round(((input_temperature - 32) * (5 / 9)) + 273.15, 2)
+            kelvin_temperature = round(((input_temperature - 32) * (5 / 9)) + self.KELVIN_TEMPERATURE, 2)
             logging.debug("kelvin temperature is {}".format(kelvin_temperature))
             return kelvin_temperature
         except TypeError:
@@ -136,7 +143,7 @@ class QuantityMeasurementSystem(IQuantityMeasurementSystem):
         :return: converted fahrenheit temperature
         """
         try:
-            fahrenheit_temperature = round(((input_temperature - 273.15) * (9 / 5)) + 32, 2)
+            fahrenheit_temperature = round(((input_temperature - self.KELVIN_TEMPERATURE) * (9 / 5)) + 32, 2)
             logging.debug("fahrenheit temperature is {}".format(fahrenheit_temperature))
             return fahrenheit_temperature
         except TypeError:
